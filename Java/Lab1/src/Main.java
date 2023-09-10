9d231b852d866ec447ecd4e41b449f41f483877b
marinnegai <negaimarin@gmail.com>
2023-09-10 16:34:03 +0300

    Java lab1

---
+++ Java/Lab1/src/Main.java
@@ -0,0 +1,28 @@
+import Guitar.Guitar;
+
+public class Main{
+    public static void main(String[] args){
+        //Create a new Guitar object
+        Guitar myGuitar =  new Guitar("Fender", "Stratocaster", "Sunburst");
+
+        //Access and print attributes
+        System.out.println("Brand: " + myGuitar.getBrand());
+        System.out.println("Model: " + myGuitar.getModel());
+        System.out.println("Color: " + myGuitar.getColor());
+
+        // Call methods
+        myGuitar.playChord("G major");
+        myGuitar.tune();
+
+        // Update attributes using the setter
+        myGuitar.setBrand("Gibson");
+        myGuitar.setModel("Les-paul");
+        myGuitar.setColor("Cherry Sunburst");
+
+        // Print updated attributes
+        System.out.println("\nUpdated Attributes:");
+        System.out.println("Brand: " + myGuitar.getBrand());
+        System.out.println("Model: " + myGuitar.getModel());
+        System.out.println("Color: " + myGuitar.getColor());
+    }
+}
