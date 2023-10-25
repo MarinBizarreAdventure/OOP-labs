8a657022f7541cbb25df452b21b5a8d664bb6704
marinnegai <negaimarin@gmail.com>
2023-10-25 21:16:14 +0300

    Record model

--- Java/Lab3/src/Main.java
+++ Java/Lab3/src/Main.java
@@ -1,5 +1,98 @@
+import java.io.IOException;
+import java.nio.file.*;
+
+
+
+import java.nio.file.*;
+
+class FileMonitor {
+    private WatchService watchService;
+
+    public FileMonitor(String directoryPath) throws IOException {
+        this.watchService = FileSystems.getDefault().newWatchService();
+        Path directory = Path.of(directoryPath);
+        directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.ENTRY_DELETE);
+    }
+
+    public void startMonitoring() {
+        try {
+            while (true) {
+                WatchKey watchKey = watchService.take();
+                for (WatchEvent<?> event : watchKey.pollEvents()) {
+                    WatchEvent<Path> pathEvent = (WatchEvent<Path>) event;
+                    Path fileName = pathEvent.context();
+                    WatchEvent.Kind<?> kind = event.kind();
+
+                    handleEvent(kind, fileName);
+                }
+                watchKey.reset();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void handleEvent(WatchEvent.Kind<?> kind, Path fileName) {
+        // Subclasses will implement this method to handle specific events.
+    }
+}
+
+class FileChangeMonitor extends FileMonitor {
+    public FileChangeMonitor(String directoryPath) throws IOException {
+        super(directoryPath);
+    }
+
+    @Override
+    protected void handleEvent(WatchEvent.Kind<?> kind, Path fileName) {
+        if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+            System.out.println("A file has been modified: " + fileName);
+        }
+    }
+}
+
+class FileCreateMonitor extends FileMonitor {
+    public FileCreateMonitor(String directoryPath) throws IOException {
+        super(directoryPath);
+    }
+
+    @Override
+    protected void handleEvent(WatchEvent.Kind<?> kind, Path fileName) {
+        if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+            System.out.println("A new file is created: " + fileName);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        try {
+//            String directoryPath = "D:\\secrete\\univer\\anul2\\labs\\Java\\Lab3\\test";
+//            FileChangeMonitor changeMonitor = new FileChangeMonitor(directoryPath);
+//            FileCreateMonitor createMonitor = new FileCreateMonitor(directoryPath);
+//            Thread changeThread = new Thread(changeMonitor::startMonitoring);
+//            Thread createThread = new Thread(createMonitor::startMonitoring);
+//            changeThread.start();
+//            createThread.start();
+//            changeThread.join();
+//            createThread.join();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        // Create a Path for a sample file
+        Path filePath = Path.of("D:\\secrete\\univer\\anul2\\labs\\Java\\Lab3\\test\\gay.txt");
+
+        // Create a Record object
+        models.Record record = new models.Record(filePath);
+        // Verify if the getExtension method works
+        System.out.println("File Extension: " + record.getExtension());
+
+        // Verify if the getCreationTime method works
+        System.out.println("Creation Time: " + record.getCreationTime());
+
+        // Verify if the getModificationTime method works
+        System.out.println("Modification Time: " + record.getModificationTime());
     }
 }
---
+++ Java/Lab3/src/models/Record.java
@@ -0,0 +1,48 @@
+package models;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Record extends File {
+    private final String extension;
+    public Record(Path path) {
+        super(path.toString());
+        int Lastindex = getName().lastIndexOf(".");
+        extension = (Lastindex > 0) ? getName().substring(Lastindex +1) : "";
+    }
+
+    public String getExtension(){
+        return extension;
+    }
+
+    public String getCreationTime(){
+        try {
+            FileTime fileTime = java.nio.file.Files.readAttributes(toPath(), BasicFileAttributes.class).creationTime();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = new Date(fileTime.toMillis());
+            return sdf.format(date);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getModificationTime(){
+        try {
+            FileTime fileTime = java.nio.file.Files.readAttributes(toPath(), BasicFileAttributes.class).lastModifiedTime();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = new Date(fileTime.toMillis());
+            return sdf.format(date);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+
