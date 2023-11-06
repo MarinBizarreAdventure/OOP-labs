357a751900668c18d22756860a42a3d8dc62fe5c
marinnegai <negaimarin@gmail.com>
2023-11-06 22:08:30 +0200

    First commit lab 4 and interface definition

---
+++ Java/Lab4/interfaces/Queue.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Queue<E> {
+    void enqueue(E element);
+    E dequeue();
+    E peek();
+    int size();
+    boolean isEmpty();
+}
---
+++ Java/Lab4/interfaces/Stack.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Stack<E> {
+    void push(E element);
+    E pop();
+    E peek();
+    int size();
+    boolean isEmpty();
+}
