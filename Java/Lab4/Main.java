aeea98f561e488a35e355391ddff1c3b1d498a21
marinnegai <negaimarin@gmail.com>
2023-11-06 23:39:04 +0200

    LinkedStack implementation

--- Java/Lab4/Main.java
+++ Java/Lab4/Main.java
@@ -1,30 +1,56 @@
-import stack.ArrayStack;
+import stack.*;
 public class Main {
         public static void main(String[] args) {
             // Create a dynamic array stack
-            ArrayStack<Integer> stack = new ArrayStack<>();
+            ArrayStack<Integer> arrayStack = new ArrayStack<>();
 
             // Push elements onto the stack
-            stack.push(5);
-            stack.push(10);
-            stack.push(15);
+            arrayStack.push(5);
+            arrayStack.push(10);
+            arrayStack.push(15);
 
             // Test pop and peek operations
-            System.out.println("Top element: " + stack.peek()); // Should print 15
-            System.out.println("Popped element: " + stack.pop());  // Should print 15
+            System.out.println("Top element: " + arrayStack.peek()); // Should print 15
+            System.out.println("Popped element: " + arrayStack.pop());  // Should print 15
 
             // Test size and isEmpty
-            System.out.println("Size of the stack: " + stack.size());  // Should print 2
-            System.out.println("Is the stack empty? " + stack.isEmpty()); // Should print false
+            System.out.println("Size of the stack: " + arrayStack.size());  // Should print 2
+            System.out.println("Is the stack empty? " + arrayStack.isEmpty()); // Should print false
 
             // Push more elements to trigger dynamic resizing
             for (int i = 0; i < 20; i++) {
-                stack.push(i * 5);
+                arrayStack.push(i * 5);
             }
 
             // Test size after dynamic resizing
-            System.out.println("Size of the stack after resizing: " + stack.size());// Should print 22
+            System.out.println("Size of the stack after resizing: " + arrayStack.size());// Should print 22
+            arrayStack.clear();
+            System.out.println("Size of the stack after clearing: " + arrayStack.size());
+
+
+
+            System.out.println("LinkedStack");
+            LinkedStack<Integer> stack = new LinkedStack<>();
+
+            // Push elements onto the stack
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            // Test peek
+            System.out.println("Peek: " + stack.peek()); // Output: Peek: 3
+
+            // Test size and isEmpty
+            System.out.println("Stack size: " + stack.size()); // Output: Stack size: 3
+            System.out.println("Is the stack empty? " + stack.isEmpty()); // Output: Is the stack empty? false
+
+            // Test pop
+            System.out.println("Pop: " + stack.pop()); // Output: Pop: 3
+
+            // Test clear
             stack.clear();
-            System.out.println("Size of the stack after clearing: " + stack.size());
+            System.out.println("Stack size after clearing: " + stack.size()); // Output: Stack size after clearing: 0
+            System.out.println("Is the stack empty after clearing? " + stack.isEmpty()); // Output: Is the stack empty after clearing? true
+
         }
 }
